f903a07c19a22facd11d3451c308554d626260c2
Blues <i@iblues.name>
2019-09-16 17:48:29 +0800

    repository化

---
+++ app/Repositories/Test/TestRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories\Test;
+
+use App\Models\Test\TestTest;
+use Prettus\Repository\Criteria\RequestCriteria;
+use App\Validators\Test\TestValidator;
+use Larfree\Repositories\LarfreeRepository;
+
+/**
+ * Class TestRepositoryEloquent.
+ *
+ * @package namespace App\Repositories\Test;
+ */
+class TestRepository extends LarfreeRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return TestTest::Class;
+    }
+
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+//        $this->pushCriteria(app(RequestCriteria::class));
+    }
+
+}

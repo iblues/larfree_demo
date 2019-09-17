a3e334562d305e76b1ecd3ecc4bd5242af7fde7a
Blues <i@iblues.name>
2019-09-17 17:30:48 +0800

    菜单完善

--- app/Repositories/Admin/AdminNavRepository.php
+++ app/Repositories/Admin/AdminNavRepository.php
@@ -39,8 +39,12 @@ class AdminNavRepository extends LarfreeRepository
      * @param int $tree 树状结构
      * @return array
      */
-    public function getAdminNav($tree=1){
-        $nav = $this->model->where('status',1)->orderBy('ranking','desc')->get();
+    public function getAdminNav($tree=1,$onlyStatus=1){
+
+        if($onlyStatus)
+            $this->model->where('status',1);
+
+        $nav = $this->model->orderBy('ranking','desc')->get();
         $nav = $nav->toArray();
         if(!$tree)
             return $nav;

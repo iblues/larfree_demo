805b4fcdf6013bbabfa380f0b230c4569eb2f8d4
Blues <i@iblues.name>
2019-09-24 11:11:05 +0800

    修正bug

--- app/Http/Controllers/Admin/Common/UserController.php
+++ app/Http/Controllers/Admin/Common/UserController.php
@@ -5,12 +5,17 @@
  */
 namespace App\Http\Controllers\Admin\Common;
 
+use App\Repositories\User\CommonUserRepository;
 use Illuminate\Http\Request;
 use Larfree\Controllers\AdminApisController as Controller;
-use App\Repositories\Common\CommonUserRepository;
 use App\Services\Common\CommonUserService;
 class UserController extends Controller
 {
+    /**
+     * UserController constructor.
+     * @param CommonUserRepository $repository
+     * @param CommonUserService $service
+     */
     public function __construct(CommonUserRepository $repository, CommonUserService $service )
     {
         $this->repository = $repository;
--- app/Http/Controllers/Admin/Test/TestController.php
+++ app/Http/Controllers/Admin/Test/TestController.php
@@ -7,6 +7,7 @@
 namespace App\Http\Controllers\Admin\Test;
 
 use App\Repositories\Test\TestTestRepository;
+use App\Services\Test\TestTestService;
 use Illuminate\Http\Request;
 use Larfree\Controllers\AdminApisController as Controller;
 
@@ -47,7 +48,7 @@ class TestController extends Controller
     ];
 
 
-    public function __construct(TestTestRepository $repository  , TestTestRepository $service)
+    public function __construct(TestTestRepository $repository  , TestTestService $service)
     {
         $this->repository = $repository;
         $this->service = $service;

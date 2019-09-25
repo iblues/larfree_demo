127201ea9431650faa5400479e99dc1330da683e
Blues <i@iblues.name>
2019-09-25 21:18:24 +0800

    更新扩展包,fix bug

--- routes/apiAdmin.php
+++ routes/apiAdmin.php
@@ -14,7 +14,6 @@ use Illuminate\Http\Request;
 
 //测试用
 Route::resource('test/test', 'Test\TestController');
-Route::resource('test/test', 'Test\TestController');
 
 Route::resource('common/user', 'Common\UserController');//用户管理
 Route::post('user/session', 'User\SessionController@store');//后台登录

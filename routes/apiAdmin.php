cf3b478ab7fc4de3f7dd19ed33485de8557dc8e7
Blues <i@iblues.name>
2019-11-10 23:50:40 +0800

    fix路由

--- routes/apiAdmin.php
+++ routes/apiAdmin.php
@@ -19,7 +19,7 @@ Route::middleware('api.auth')->group(function () {
     Route::resource('common/user', 'Admin\Common\UserController');//用户管理
     Route::get('user/admin/{id}', 'Admin\Common\UserController@show');//获取当前用户信息
     Route::post('user/session', 'Admin\User\SessionController@store');//后台登录
-    Route::delete('user/session', 'User\SessionController@loginout');//后台退出
+    Route::delete('user/session', 'Admin\User\SessionController@loginout');//后台退出
 
     Route::get('admin/nav/tree', 'Admin\Admin\NavController@tree');//树桩导航
     Route::resource('admin/nav', 'Admin\Admin\NavController');//导航管理

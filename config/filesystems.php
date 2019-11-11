e8f181834375f45e10acde85f8abeaf19638f18a
Blues <i@iblues.name>
2019-11-11 21:35:59 +0800

    oss

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -63,6 +63,21 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
         ],
+
+
+        'oss' => [
+            'driver' => 'oss',
+            'access_id' => env('ALI_ACCESS_KEY_ID', ''),
+            'access_key' => env('ALI_ACCESS_KEY_SECRET', ''),
+            'bucket' => env('ALI_BUCKET', ''),
+            'endpoint' => env('ALI_END_POINT', ''), // OSS 外网节点或自定义外部域名
+            //'endpoint_internal' => '<internal endpoint [OSS内网节点] 如：oss-cn-shenzhen-internal.aliyuncs.com>', // v2.0.4 新增配置属性，如果为空，则默认使用 endpoint 配置(由于内网上传有点小问题未解决，请大家暂时不要使用内网节点上传，正在与阿里技术沟通中)
+            'cdnDomain' => env('ALI_OSS_CDN_DOMAIN', ''), // 如果isCName为true, getUrl会判断cdnDomain是否设定来决定返回的url，如果cdnDomain未设置，则使用endpoint来生成url，否则使用cdn
+            'ssl' => env('ALI_OSS_HTTPS', 'false'), // true to use 'https://' and false to use 'http://'. default is false,
+            'isCName' => env('ALI_OSS_CDN_DOMAIN', '') ? true : false, // 是否使用自定义域名,true: 则Storage.url()会使用自定义的cdn或域名生成文件url， false: 则使用外部节点生成url
+            'debug' => env('ALI_OSS_DEBUG', false),
+        ],
+
 
     ],
 
